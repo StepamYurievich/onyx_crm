4c47fcec8526269fef015c3293ea32df0dcf9249
ssassos <sbuharov45@gmail.com>
2024-10-14 17:22:09 +0500

    a

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles/navbar.sass'
-import logo from "../../assets/Group 40448.svg";
 import {Link} from "react-router-dom";
 import Accordion from "./NavBarItems/Accordion/Accordion.jsx";
 const NavBar = () => {
@@ -8,7 +7,6 @@ const NavBar = () => {
     return (
         <div className={active ? 'header-active' : 'menu'}>
             <ul className={"header_ul"}>
-                <li><img src={logo} alt="logo"/></li>
                 <li><Link to={'/'}></Link></li>
                 <button onClick={()=> setActive(!active)}>{'>'}</button>
             </ul>
